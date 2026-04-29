cca97ef25d3a7fcf9989ce0152f17d8a4447cdcf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-29 01:54:55 +0000

    motionik 2.0.9

--- Casks/m/motionik.rb
+++ Casks/m/motionik.rb
@@ -1,9 +1,9 @@
 cask "motionik" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.0.8"
-  sha256 arm:   "a0333efafa0ce8ed0ab7275254cbc95e3b5def3c85d996be4acdbafcc86dfe66",
-         intel: "593465b3c7f1006b6a9c35c83917040a5711cbe5cfcb6509fe7a343d3472d1b2"
+  version "2.0.9"
+  sha256 arm:   "42e70b4b0e1ffe2e473fef34602e17be00357a3e7bafbbb8e72ee55beffe1822",
+         intel: "5e2f9a9dd4fcef44352a72f090ebfcb782cdd0a2c7251324acf8d3c0cebef2d8"
 
   url "https://assets.motionik.com/releases/Motionik-#{version}-#{arch}.dmg"
   name "Motionik"
