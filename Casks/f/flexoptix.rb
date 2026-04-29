369821594060e944b28cde30219d13fbe8c173b4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-29 13:19:13 +0000

    flexoptix 5.63.0-latest

--- Casks/f/flexoptix.rb
+++ Casks/f/flexoptix.rb
@@ -2,9 +2,9 @@ cask "flexoptix" do
   arch arm: "arm64", intel: "x64"
   arch_suffix = on_arch_conditional arm: "-arm64"
 
-  version "5.62.0-latest"
-  sha256 arm:   "463219e8ddeb6b94445a7cabd6ccc1aded9ae9eea60fe4f818f72714f529397a",
-         intel: "05ebf15525aa07c2a62c100dc2a03530a680209691d79deda2742c436a00780b"
+  version "5.63.0-latest"
+  sha256 arm:   "61d02acc6e3cea45fb9c2d61be7cfcc0ae52dd0431d16d7d553a127ae2607630",
+         intel: "153962cba300e51ea0724880ddd8765a971ad399e9987f5a58b63f2312d14a29"
 
   url "https://flexbox.reconfigure.me/download/electron/mac/#{arch}/FLEXOPTIX%20App-#{version}#{arch_suffix}.dmg",
       verified: "flexbox.reconfigure.me/download/electron/mac/"
