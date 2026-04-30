97d162d88043dd1e0d5ff27a001de5c2e2495fe6
Bevan Kay <email@bevankay.me>
2026-04-30 21:18:33 +1000

    kotlin-lsp: use `os` for now, to enable linux CI

--- Casks/k/kotlin-lsp.rb
+++ Casks/k/kotlin-lsp.rb
@@ -1,5 +1,6 @@
 cask "kotlin-lsp" do
   arch arm: "-aarch64"
+  os macos: "sit", linux: "tar.gz"
 
   version "262.4739.0"
   sha256 arm:          "1b745743ce22ad92681a1bc3b1046803e942a6e1f36e04fb85ae9a40334a2f1e",
@@ -7,15 +8,8 @@ cask "kotlin-lsp" do
          arm64_linux:  "625870ae091c6d0dee25514d545c708a6ea50d7cbb5154aaf1aa9123ccff338b",
          x86_64_linux: "46971110c9b8a3360ce3fdf5437467f4c447dad37ad73dbf81d64af6779e4105"
 
-  on_macos do
-    url "https://download-cdn.jetbrains.com/kotlin-lsp/#{version}/kotlin-server-#{version}#{arch}.sit",
-        verified: "download-cdn.jetbrains.com/kotlin-lsp/"
-  end
-  on_linux do
-    url "https://download-cdn.jetbrains.com/kotlin-lsp/#{version}/kotlin-server-#{version}#{arch}.tar.gz",
-        verified: "download-cdn.jetbrains.com/kotlin-lsp/"
-  end
-
+  url "https://download-cdn.jetbrains.com/kotlin-lsp/#{version}/kotlin-server-#{version}#{arch}.#{os}",
+      verified: "download-cdn.jetbrains.com/kotlin-lsp/"
   name "Kotlin LSP"
   desc "Official Kotlin Language Server"
   homepage "https://github.com/Kotlin/kotlin-lsp"
